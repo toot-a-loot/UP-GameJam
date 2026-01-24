0fd9c4340d75d45cd864cb998098267fc1293c3b
HoogaBoga <matty.lim718@gmail.com>
2026-01-24 14:11:57 +0800

    Created player mechanics

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
+@export var speed = 300.0
+@onready var hands_blackout = $CanvasLayer/ColorRect
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(delt):
+	# Mechanic: are we covering eyes?
+	if Input.is_action_pressed("cover_eyes"):
+		hands_blackout.visible = true
+		velocity = Vector2.ZERO
+	
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		hands_blackout.visible = false
+		move_normally()
+		
 	move_and_slide()
+	
+func move_normally():
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if direction:
+		velocity = direction * speed
+	
+	else:
+		velocity = Vector2.ZERO
