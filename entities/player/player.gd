2308a24ffbc9c24fa07af01ec0493202d59d761f
DrineDev <ejcotejar@gmail.com>
2026-01-26 10:58:47 +0800

    feature: minimap

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -12,6 +12,15 @@ extends CharacterBody3D
 @onready var camera = $Head/Camera3D
 @onready var flashlight = $Head/OmniLight3D
 @onready var blindfold = $CanvasLayer/ColorRect
+# MAP NODES
+@onready var minimap_container = $CanvasLayer/MinimapContainer
+@onready var map_texture_rect = $CanvasLayer/MinimapContainer/MapTexture
+@onready var player_marker = $CanvasLayer/MinimapContainer/MapTexture/PlayerMarker
+
+# Minimap data
+var map_width_cells: float = 21.0
+var map_height_cells: float = 21.0
+var cell_size_world: float = 7.0 # grid size
 
 # STATE VARIABLE
 # false = Sight Mode (Can see, can't move)
@@ -71,9 +80,12 @@ func _physics_process(delta):
 		velocity.z = 0
 
 	move_and_slide()
+	
+	# MINIMAP
+	check_looking_down()
+	update_minimap_marker()
 
 # --- VISUAL HELPERS ---
-
 func set_state_moving():
 	# Darken screen, allow flashlight
 	blindfold.visible = true
@@ -85,3 +97,51 @@ func set_state_sight():
 	blindfold.visible = false
 	blindfold.color.a = 0.0
 	flashlight.visible = true
+
+func initialize_minimap(map_data: Array, w: int, h: int):
+	map_width_cells = float(w)
+	map_height_cells = float(h)
+	
+	var img = Image.create(w, h, false, Image.FORMAT_RGBA8)
+	for x in range(w):
+		for y in range(h):
+			if map_data[x][y] == 1:
+				img.set_pixel(x, y, Color.BLACK)
+			else:
+				img.set_pixel(x, y, Color.LIGHT_GRAY)
+				
+	var tex = ImageTexture.create_from_image(img)
+	map_texture_rect.texture = tex
+	
+func check_looking_down():
+	var _forward = camera.global_transform.basis.z
+	
+	var look_dir = -camera.global_transform.basis.z
+	var dot = look_dir.dot(Vector3.DOWN)
+	
+	if dot > 0.85:
+		minimap_container.visible = true
+	else:
+		minimap_container.visible = false
+		
+func update_minimap_marker():
+	if not minimap_container.visible: return
+	
+	# Calculate Grid Position
+	var grid_x = global_position.x / cell_size_world
+	var grid_z = global_position.z / cell_size_world
+	
+	# Calculate Ratio using the DYNAMIC map size
+	var ratio_x = grid_x / map_width_cells
+	var ratio_y = grid_z / map_height_cells
+	
+	# Map to UI Size
+	var ui_width = map_texture_rect.size.x
+	var ui_height = map_texture_rect.size.y
+	
+	# Set Position
+	var final_x = (ratio_x * ui_width) - (player_marker.size.x / 2.0)
+	var final_y = (ratio_y * ui_height) - (player_marker.size.y / 2.0)
+	
+	player_marker.position.x = final_x
+	player_marker.position.y = final_y
